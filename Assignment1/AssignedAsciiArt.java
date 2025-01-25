23c619b36e5f289b34dbf9d8952778ca35c7c03a
WiiliamMentzer <william.mentzer2k@gmail.com>
2025-01-25 13:07:46 -0800

    Updated FreeFormAsciiArt and AssignedAsciiArt. Bug testing Assigned, WIP

--- Assignment1/AssignedAsciiArt.java
+++ Assignment1/AssignedAsciiArt.java
@@ -1,3 +1,138 @@
+import java.util.*;
 public class AssignedAsciiArt {
-    
+
+    public static void needleCore(int size){
+        for(int i=0; i < size ; i++){
+            for(int j=0; j < size * 6; j++){
+                if(j == size * 3){
+                    System.out.print("||");
+                }
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void needleRoofCap(int size){
+        int layer = 0;
+        for(int i=0; i < size; i++){
+            for(int j=0; j < size*4; j++){
+                if(j==0 || j == size*3){
+                    //If beginning or end of roofcap build
+                    for(int k=0; k < (size*2) - (layer*3); k++){
+                        /*Size:3
+                        first k up to 6 spaces created
+                        second k up to 3
+                        third k 0
+                        */
+                        System.out.print(" ");
+                    }
+                } else if(j == size*2){
+                    //if not beginning or end, build the structure.
+                    System.out.print("__/");
+                    if(layer > 0){
+                        //If size is greater than 1 ,pad with colons before fnishing the roofCap
+                        for(int k=0; k < layer; k++){
+                            System.out.print(":::");
+                        }
+                    }
+
+                    System.out.print("||");
+                    //Handle reverse of top half of method
+                    if(layer > 0){
+                        for(int k=0; k < layer; k++){
+                            System.out.print(":::");
+                        }
+                    }
+                    System.out.print("\\__");
+                }
+            }
+            System.out.println("");
+            layer++;
+            //Increase in layer to handle varying sizes
+        }
+    }
+    public static void needleBridge(int size){
+        for(int i=1; i < size*7; i++){
+            if( i==1 || i == size*7-1){
+                System.out.print("|");
+            }else{
+                System.out.print("\"");
+            }
+        }
+        System.out.println("");
+    }
+
+    public static void needleUnderCap(int size){
+        int layer = size;
+        for(int i=0; i < size; i++){
+            for(int j=0; j < size*layer; j++){
+                if(j==0 || j == size*layer-1){
+                    //If beginning or end of UnderCap build on more the second layer or more
+                    for(int k=0; k < (size-layer)*2 ; k++){
+                        /*Size:3
+                        Layer 1 should have 0 spaces created
+                        Layer 2 should be 2 spaces
+                        Layer 3 should be 4 spaces
+                        */
+                        System.out.print(" ");
+                    }
+                } else{
+                    System.out.print("\\_/");
+                    for(int l=0; l < (size*layer)-(1*(layer-1)) ; l++){
+                        /*
+                        Size 3
+                        Layer 1 build 7
+                        Layer 2 build 5
+                        Layer 3 build 3(size)
+                        */
+                        System.out.print("\\/");
+                        j++;
+                    }
+                    System.out.print("\\_/");
+                }
+            }
+            System.out.println("");
+            layer--;
+        }
+    }
+
+    public static void needleElevator(int size){
+        for(int i=0; i < size*size ; i++){
+            for(int j=0; j < (size * 6)-2; j++){
+
+
+                if(j == size * 3){
+                    System.out.print("||");
+                }else if(j > (size*2)+1 && j < (size*4)-1){
+                    System.out.print("%");
+                }else if(j==(size*2)+1 || j == (size*4)-1){
+                    System.out.print("|");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static int gatherSize(Scanner input, int size){
+        System.out.print("What size space needle would you like to see? ");
+        size = input.nextInt();
+        return size;
+    }
+
+    public static void main(String[] args){
+        int size = 0;
+        Scanner input = new Scanner(System.in);
+        size = gatherSize(input, size);
+        needleCore(size);
+        needleRoofCap(size);
+        needleBridge(size);
+        needleUnderCap(size);
+        needleCore(size);
+        needleElevator(size);
+        needleRoofCap(size);
+        needleBridge(size);
+    }
 }
--- Assignment1/FreeFormAsciiArt.java
+++ Assignment1/FreeFormAsciiArt.java
@@ -1,3 +1,53 @@
+import java.util.*;
+/*
+Freeform Ascii Art By William Mentzer
+For: CS-142 Intermediate Programming
+
+Purpose: To create a creeper face using the " " and * in output, letting the user increase its size based on user input
+*/
 public class FreeFormAsciiArt {
-    
+
+    public static void creepFill(int size){
+        for(int c = 0; c < size * 2; c++){
+            System.out.print("*");
+        }
+    }
+
+    public static void creepEmpty(int size){
+        for(int c = 0; c < size * 2; c++){
+            System.out.print(" ");
+        }
+    }
+
+    public static void main(String[] args){
+        int [][] blueprintCreeper = {
+        {0,0,0,0,0,0,0,0},
+        {0,1,1,0,0,1,1,0},
+        {0,1,1,0,0,1,1,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,1,1,0,0,0},
+        {0,0,1,1,1,1,0,0},
+        {0,0,1,0,0,1,0,0},
+        {0,0,0,0,0,0,0,0},
+        };
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("How wide do you want the creeper face to be?");
+
+        int size = 1;
+        size = input.nextInt();
+
+        //This nested for loop looks parses through every index in the array and looks for a binary 0 or 1 whether it should or should not create an empty " " sector
+        for(int i=0; i < blueprintCreeper.length; i++){
+            for(int j=0; j < blueprintCreeper[i].length; j++){
+                if (blueprintCreeper[i][j] == 1){
+                    creepEmpty(size);
+                } else {
+                    creepFill(size);
+                }
+            }
+            System.out.println("");
+        }
+    }
+
 }
